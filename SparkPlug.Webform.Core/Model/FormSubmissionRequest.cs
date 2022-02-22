2f131523978bc44010b1ed4b6797e775a31933b8
Tiamiyu Kehinde <Tiamiyukehinde5@gmail.com>
2022-02-22 14:03:17 +0100

    Mad changes to model

--- SparkPlug.Webform.Core/Model/FormSubmissionRequest.cs
+++ SparkPlug.Webform.Core/Model/FormSubmissionRequest.cs
@@ -9,23 +9,6 @@ namespace SparkPlug.WebForm.Core.Model
 {
     public class FormSubmissionRequest
     {
-        //[Required]
-        //public string CustomerName { get; set; }
-
-        //[Required]
-        //[EmailAddress]
-
-        //public string CustomerEmail { get; set; }
-
-        //[Required]
-
-        //public string CustomerMessage { get; set; }
-
-
-        //public string _formName { get; set; }
-
-
-        //public string _formDomainName { get; set; }
         [Required]
         [JsonPropertyName("customerName")]
         public string CustomerName { get; set; }
@@ -40,11 +23,11 @@ namespace SparkPlug.WebForm.Core.Model
         public string CustomerMessage { get; set; }
 
         [Required]
-        //[JsonPropertyName("_formName")]
+
         public string _formName { get; set; }
 
         [Required]
-        //[JsonPropertyName("_formDomainName")]
+
         public string _formDomainName { get; set; }
     }
 }
